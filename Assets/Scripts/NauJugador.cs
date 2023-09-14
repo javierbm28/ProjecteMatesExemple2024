a4653325f09f6eda7982cc6c8a303e27eb7af167
Javierbm28 <javier_ericbravo@ceroca.cat>
2023-09-14 19:43:48 +0200

    Moviment Horizontal Nau

---
+++ Assets/Scripts/NauJugador.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NauJugador : MonoBehaviour
+{
+
+    [SerializeField] private float _velNau;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _velNau = 5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float direccioHorizontal = Input.GetAxisRaw("Horizontal");
+        //Debug.Log(direccioHorizontal);
+
+        Vector2 direccioIndicada = new Vector2(direccioHorizontal, 0f);
+        
+        Vector2 novaPos = transform.position; //Ens retorna la posició actual de la nau.
+
+        novaPos += direccioIndicada * _velNau * Time.deltaTime;
+
+        transform.position = novaPos;
+    }
+}
